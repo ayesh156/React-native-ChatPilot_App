204efefbb65f6df143574406d081f932a1c8c706
= <=>
2024-09-28 21:16:33 +0530

    Second Commit

--- Programming Part/chat_pilot/chat_update.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$id = $_GET["id"];
-
-$connection = new mysqli("localhost","root","SEngineer,531","chatpilot");
-$connection->query("UPDATE `chat` SET `status_id` = '2' WHERE `user_from_id` = '".$id."'");
-
-?>
